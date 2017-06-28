c8b8b73eec4913523e1cb383c3339b1788ae8435
mzdixon1 <mzdixon1@yahoo.com>
2017-06-28 13:45:12 -0400

    second commit

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,29 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the name on the reservation");
+            string reserveName = Console.ReadLine();
+
+            Console.WriteLine("How many people in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            double amountEach = 13.00d;
+
+            double totalBill = numberOfPeople * amountEach;
+            Console.WriteLine("Reservation: name" + "reserveName"); //concatenate
+            Console.WriteLine("Total due: $" + totalBill);
+
+            if(totalBill >= 50)
+            {
+                double totaldue = totalBill * 1.1;
+                Console.WriteLine("You will get a 10% discount");
+            }
+            else
+            {
+                Console.WriteLine("You will get a 5% discount");
+
+            }
+            Console.WriteLine("total due: 65 - 10% discount");
         }
     }
 }
