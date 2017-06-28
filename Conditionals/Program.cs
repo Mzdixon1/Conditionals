34b8910e7aa5aae441768afe5c0e664a7c4dabf1
mzdixon1 <mzdixon1@yahoo.com>
2017-06-28 15:03:36 -0400

    switch case practice

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -9,30 +9,72 @@ namespace Conditionals
     class Program
     {
         static void Main(string[] args)
+        //        {
+        //            Console.WriteLine("What is the name on the reservation");
+        //            string reserveName = Console.ReadLine();
+
+        //            Console.WriteLine("How many people in your party?");
+        //            int numberOfPeople = int.Parse(Console.ReadLine());
+
+        //            double amountEach = 13.00d;
+
+        //            double totalBill = numberOfPeople * amountEach;
+        //            Console.WriteLine("Reservation: name" + "reserveName"); //concatenate
+        //            Console.WriteLine("Total due: $" + totalBill);
+
+        //            double totalBill = double.Parse(Console.ReadLine());
+
+        //            if (totalBill >= 50)
+        //            {
+
+        //                Console.WriteLine("You will get a 10% discount");
+        //                Console.WriteLine("Your total with discount: $" + discountTen);
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("You will get a 5% discount");
+        //                Console.WriteLine("Your total with discount: $" + discountFive);
+        //            }
+        //            Console.WriteLine("total due: 65 - 10% discount");
+        //        }
+        //    }
+        //}
         {
-            Console.WriteLine("What is the name on the reservation");
-            string reserveName = Console.ReadLine();
-
-            Console.WriteLine("How many people in your party?");
-            int numberOfPeople = int.Parse(Console.ReadLine());
-
-            double amountEach = 13.00d;
-
-            double totalBill = numberOfPeople * amountEach;
-            Console.WriteLine("Reservation: name" + "reserveName"); //concatenate
-            Console.WriteLine("Total due: $" + totalBill);
-
-            if(totalBill >= 50)
+            Console.WriteLine("What is your age");
+            int age = int.Parse(Console.ReadLine());
+             if (age >= 0 && age <= 2)
             {
-                double totaldue = totalBill * 1.1;
-                Console.WriteLine("You will get a 10% discount");
-            }
-            else
-            {
-                Console.WriteLine("You will get a 5% discount");
+                Console.WriteLine("Still in Mama's Arms");
 
             }
-            Console.WriteLine("total due: 65 - 10% discount");
+            else if (age == 3 || age == 4)
+            {
+                Console.WriteLine("Preschool Maniac");
+            }
+            else if (age >= 5 && age <= 11)
+            {
+                Console.WriteLine("Elementary School");
+            }
+            else if (age >= 12 && age <= 14)
+            {
+                Console.WriteLine("Middle School");
+            }
+            else if (age >= 15 && age <= 18)
+            {
+                Console.WriteLine("High School");
+            }
+            else if (age >= 19 && age <= 22)
+            {
+
+            }
+            else if (age >= 23 && age <= 65)
+            {
+                Console.WriteLine("Working for the Man");
+            } 
+            else if (age >= 66 && age <= 100)
+            {
+                Console.WriteLine("The Golden Years");
+            }
         }
     }
 }
